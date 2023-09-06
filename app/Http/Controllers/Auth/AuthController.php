974fdb350c5e63d6815d7e96060edde2441de6ab
Marcelino <marcelinomebe@gmail.com>
2023-09-06 18:16:40 -0500

    Se ha creado un controlador llamado 'AuthController' con tres métodos: 1: register 2: login 3: logout Se han implementado rutas correspondientes en 'routes/api.php' y se ha vinculado el controlador a estas rutas.

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        
+    }
+
+    public function login()
+    {
+
+    }
+
+    public function logout()
+    {
+
+    }
+}
