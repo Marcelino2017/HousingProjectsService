a4b9337c149275bddf7e137cf346cc2dda97610f
Marcelino <marcelinomebe@gmail.com>
2023-09-06 20:46:15 -0500

    se ha desarrollado la funcionalidad de inicio de sesión de usuarios que ahora retorna un token de autenticación válido y los datos del usuario al hacer una solicitud de inicio de sesión. Los usuarios pueden iniciar sesión proporcionando su dirección de correo electrónico y contraseña.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -7,7 +7,7 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -25,9 +25,24 @@ class AuthController extends Controller
         ];
     }
 
-    public function login()
+    public function login(LoginRequest $request)
     {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
 
+        if (!Auth::attempt($credentials, true)) {
+            return response([
+                'errors' => ['El email o  el password son incorrectos']
+            ], 422);
+        }
+
+        $user = Auth::user();
+        return [
+            'token' => $user->createToken('token')->plainTextToken,
+            'user' => $user,
+        ];
     }
 
     public function logout()
