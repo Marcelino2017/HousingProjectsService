2a99f0d5f42efb0ec6b5c581d0a78cdd42d7e604
Marcelino <marcelinomebe@gmail.com>
2023-09-06 22:11:40 -0500

    Se Crean los modoles de Payment y HousingProject

---
+++ app/Models/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'housing_project_id',
+        'payment_number',
+    ];
+}
