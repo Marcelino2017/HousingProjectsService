e7c41e0aa15f10a94618f8593a24e1be6a880281
Marcelino <marcelinomebe@gmail.com>
2023-09-07 15:24:25 -0500

    Implementar la funcionalidad de almacenamiento (store) para el controlador PaymentController, la igual que se crear la clase PaymentProjectResource para retornar el la informacion como collecciones

--- app/Http/Controllers/Payments/PaymentController.php
+++ app/Http/Controllers/Payments/PaymentController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Payments;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Payments\PaymentProjectStoreRequest;
+use App\Http\Resources\Payments\PaymentProjectResource;
+use App\Models\HousingProject;
 use App\Models\Payment;
 use Illuminate\Http\Request;
 
@@ -19,17 +22,47 @@ class PaymentController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(PaymentProjectStoreRequest $request)
     {
-        //
+        $housingProject = HousingProject::find($request->housing_project_id);
+
+        if (!$housingProject) {
+            return response()->json(['message' => 'Proyecto de vivienda no encontrado'], 404);
+        }
+
+        if ($request->amount > $housingProject->house->price) {
+            return response()->json(['message' => 'El monto a pagar supera la el precio del Proyecto de vivienda'], 404);
+        }
+
+        $payments = Payment::where('housing_project_id', $request->housing_project_id)->get();
+        $sumTotal = $payments->sum('amount') + $request->amount;
+        $totalPay = $payments->count()  + 1;
+
+        if ($sumTotal > $housingProject->house->price) {
+            return response()->json(['message' => 'El monto a pagar supera el precio total del Proyecto de vivienda'], 404);
+        }
+
+        if ($totalPay > $housingProject->payment_number) {
+            return response()->json(['message' => 'El monto a pagar supera el numero de cuota del proyecto de vivienda'], 404);
+        }
+
+        $newPayment = Payment::create([
+            'housing_project_id' => $request->housing_project_id,
+            'amount' => $request->amount,
+            'payment_date' => $request->payment_date,
+        ]);
+
+        return new PaymentProjectResource($newPayment);
     }
+
+
 
     /**
      * Display the specified resource.
      */
     public function show(Payment $payment)
     {
-        //
+        
     }
 
     /**
---
+++ app/Http/Resources/Payments/PaymentProjectResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources\Payments;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PaymentProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'housing_project_id' => $this->housing_project_id,
+            'amount' => $this->amount,
+            'payment_date' => $this->payment_date,
+            'housing_project' => $this->housingProject,
+        ];
+    }
+}
