47128846f64e2ab2afdf889ff78d98a38c2f7b47
Marcelino <marcelinomebe@gmail.com>
2023-09-07 11:34:19 -0500

    Modificación en la cantidad de digito que puede terner el campo  price en la tabla houses

--- database/migrations/2023_09_07_012558_create_houses_table.php
+++ database/migrations/2023_09_07_012558_create_houses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->integer('number_rooms');
-            $table->decimal('price', 10, 2);
+            $table->decimal('price', 30, 2);
             $table->text('description')->nullable();
             $table->timestamps();
         });
