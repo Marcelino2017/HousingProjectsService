0e730a45496e3dd04b31ea52bd34dc9f6cfc3486
Marcelino <marcelinomebe@gmail.com>
2023-09-07 14:02:22 -0500

    Se crear el request para la validación de actualizar los proyectos de viviendas

---
+++ app/Http/Requests/HousingProjects/HousingProjectUpdateRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests\HousingProjects;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class HousingProjectUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'house_id' =>  ['required', 'numeric', 'exists:houses,id', 'unique:housing_projects,house_id,'.$this->house_id],
+            'user_id' =>  ['required', 'numeric', 'exists:users,id'],
+            'payment_number' =>  ['required', 'numeric'],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'house_id.required' => 'El campo :attribute es obligatorio',
+            'house_id.unique' => 'El :attribute ya ha sido tomado.',
+            'house_id.exists' => 'El :attribute seleccionada no es válido.',
+            'house_id.numeric' => 'El campo del :attribute debe ser un número.',
+            'user_id.required' => 'El campo :attribute es obligatorio',
+            'user_id.exists' => 'El :attribute seleccionada no es válido.',
+            'user_id.numeric' => 'El campo del :attribute debe ser un número.',
+            'payment_number.required' => 'El campo:attribute es obligatorio',
+            'payment_number.numeric' => 'El campo del :attribute debe ser un número.',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'house_id' => 'Id de la casa',
+            'user_id' => 'Id del usuario',
+            'payment_number' => 'número de pago',
+        ];
+    }
+}
